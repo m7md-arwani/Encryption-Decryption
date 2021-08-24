57fcbf4c35496e6778323625397c1134a62fa983
m7md-arwani <moh.3ara@hotmail.com>
2021-08-24 16:26:01 +0300

    Input from files option, and output to file option .(Stage 5)

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,22 +1,25 @@
 package encryptdecrypt;
 
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.io.File;
 
 
 public class Main {
     public static void main(String[] args) {
         HashMap<String, String> hash = new HashMap<>();
-        String message;
+        String message = "";
+        String inPath = "";
+        String outPath = "";
         int key;
         for (int i = 0; i < args.length; i += 2) {
             hash.put(args[i], args[i + 1]);
         }
-        if ("".equals(hash.get("-data"))) {
-            message = "";
-        } else {
-            message = hash.get("-data");
-        }
+
 
         if ("".equals(hash.get("-key"))) {
             key = 0;
@@ -24,10 +27,40 @@ public class Main {
             key = Integer.parseInt(hash.get("-key"));
         }
 
+        if (hash.containsKey("-out")) {
+            outPath = hash.get("-out");
+
+        }
+
+        if (!hash.containsKey("-data") && !hash.containsKey("-in")) {
+            message = "";
+
+        } else if (hash.containsKey("-data") && hash.containsKey("-in")) {
+            message = hash.get("-data");
+
+        } else if (!hash.containsKey("-data") && hash.containsKey("-in")) {
+            inPath = hash.get("-in");
+            message = readFile(inPath);
+
+
+        }
+
+
         if ("dec".equals(hash.get("-mode"))) {
-            DecryptionAlgorithm(message, key);
+            if (!hash.containsKey("-out")) {
+                DecryptionAlgorithm(message, key);
+            } else if (hash.containsKey("-in") && hash.containsKey("-out")) {
+                DecryptionAlgorithm(message, outPath, key);
+            }
+
         } else {
-            EncryptionAlgorithm(message, key);
+            if (!hash.containsKey("-out")) {
+                EncryptionAlgorithm(message, key);
+            } else if (hash.containsKey("-in") && hash.containsKey("-out")) {
+                EncryptionAlgorithm(message, outPath, key);
+            }
+
+
         }
 
 
@@ -49,6 +82,26 @@ public class Main {
         System.out.println(str);
     }
 
+    //@Overloaded version of Encryption
+
+    public static void EncryptionAlgorithm(String mes, String out, int key) {
+        StringBuilder str = new StringBuilder(mes);// Text to cipher.
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            ch += key; // ciphering.
+            if (ch > 126) {
+                ch -= 94;
+            }
+            str.setCharAt(i, ch);
+
+        }
+        writeFile(str.toString(), out);
+
+
+    }
+
+
     public static void DecryptionAlgorithm(String st, int key) {
         StringBuilder str = new StringBuilder(st);// Text to decipher.
         for (int i = 0; i < str.length(); i++) {
@@ -63,5 +116,44 @@ public class Main {
         }
         System.out.println(str);
 
+    }
+
+    //@Overloaded version of Decryption
+
+    public static void DecryptionAlgorithm(String st, String outPath, int key) {
+        StringBuilder str = new StringBuilder(st);// Text to decipher.
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            ch -= key; // deciphering.
+            if (ch > 126) {
+                ch += 94;
+            }
+            str.setCharAt(i, ch);
+
+        }
+        writeFile(str.toString(), outPath);
+
+    }
+
+    public static String readFile(String path) {
+
+        try {
+            return new String(Files.readAllBytes(Paths.get(path)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+
+    public static void writeFile(String text, String path) {
+        try {
+            FileWriter write = new FileWriter(new File(path));
+            write.append(text);
+            write.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
